386cc9148f2d7e45f326f7dc7e1573e3c81f0610
сергей токарев <sseregatokarew@gmail.com>
2021-12-04 16:20:46 +0700

    Добавьте файлы проекта.

---
+++ REC-84/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace REC_84
+{
+    class Program
+    {
+        static BigInteger Factorial(int Num)
+        {
+            int i = 1;
+            BigInteger FinalNum = new BigInteger(1);
+            FinalNum = 1;
+            while (i <= Num) 
+            {
+                FinalNum = FinalNum * i;
+                i++;
+            }
+            return FinalNum;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int Num = int.Parse(Console.ReadLine());
+            Console.Write($"Факториал числа: {Factorial(Num)}");
+            Console.ReadKey();
+        }
+    }
+}
